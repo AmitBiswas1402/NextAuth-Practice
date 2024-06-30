9a7f73ad91951e2d7c4fb5f8c9591ff395c85578
Amit Biswas <amit.142biswas@gmail.com>
2024-06-30 13:45:22 +0530

    added forms

--- src/app/register/Form.tsx
+++ src/app/register/Form.tsx
@@ -1,9 +1,66 @@
 'use client'
 import {z} from 'zod'
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+
+const formSchema = z.object({
+  email: z
+  .string()
+  .min(2, {
+    message: "This is required field"
+  })
+  .email("This is not a valid email")
+  .max(20, {
+    message: "Password should not be more that 20 charecters"
+  }),
+  password: z.string()
+  .min(6, {message: "Password should be at least 6 charecters long"})
+})
 
 const RegisterForm = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: { },
+  })
+ 
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values)
+  }
+
   return (
-    <div>Form</div>
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input type='email' placeholder="shadcn" {...field} />
+              </FormControl>
+              <FormDescription>
+                This is your email used to sign in.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
   )
 }
 
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -2,6 +2,7 @@ import { getServerSession } from 'next-auth'
 import React from 'react'
 import { authOptions } from '../../../lib/authOptions'
 import { redirect } from 'next/navigation'
+import RegisterForm from './Form'
 
 const page = async () => {
     const session = await getServerSession(authOptions)
@@ -13,7 +14,7 @@ const page = async () => {
   return (
     <section className='h-screen flex items-center justify-center container'>
         <div className='w-[800px]'>
-
+          <RegisterForm />
         </div>
     </section>
   )
