b9c1ffb50fa26827a450a92546fb864c6adc8f92
Amit Biswas <amit.142biswas@gmail.com>
2024-07-01 21:09:08 +0530

    db made

--- src/app/register/Form.tsx
+++ src/app/register/Form.tsx
@@ -1,9 +1,11 @@
-'use client'
-import {z} from 'zod'
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
+"use client";
 
-import { Button } from "@/components/ui/button"
+import React from "react";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
@@ -12,37 +14,64 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { toast } from "sonner";
+import Link from "next/link";
 
-const formSchema = z.object({
-  email: z
-  .string()
-  .min(2, {
-    message: "This is required field"
+const formSchema = z
+  .object({
+    email: z
+      .string()
+      .min(1, {
+        message: "This field has to be filled.",
+      })
+      .email("This is not a valid email")
+      .max(300, {
+        message: "Password can't be longer than 300 characters.",
+      }),
+    password: z
+      .string()
+      .min(6, { message: "Password has to be at least 6 characters long." }),
+    confirmPassword: z.string().min(6, {
+      message: "Confirm-Password has to be at least 6 characters long.",
+    }),
   })
-  .email("This is not a valid email")
-  .max(20, {
-    message: "Password should not be more that 20 charecters"
-  }),
-  password: z
-  .string()
-  .min(6, {message: "Password should be at least 6 charecters long"})
-})
+  .superRefine(({ confirmPassword, password }, ctx) => {
+    if (confirmPassword !== password) {
+      ctx.addIssue({
+        code: "custom",
+        message: "The passwords did not match",
+        path: ["confirmPassword"],
+      });
+    }
+  });
 
 const RegisterForm = () => {
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
-    defaultValues: { },
-  })
- 
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    console.log(values)
+    defaultValues: {},
+  });
+
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    const response = await fetch(`/api/auth/register`, {
+      method: "POST",
+      body: JSON.stringify(values),
+    });
+
+    const data = await response.json();
+
+    if (data.error) {
+      toast.error(data.error);
+    }
+
+    toast.success("Account created!");
   }
 
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <h1 className="text-2xl font-semibold">Registration</h1>
         <FormField
           control={form.control}
           name="email"
@@ -50,10 +79,10 @@ const RegisterForm = () => {
             <FormItem>
               <FormLabel>Email</FormLabel>
               <FormControl>
-                <Input type='email' placeholder="your@email.com" {...field} />
+                <Input placeholder="abc@bc.com" {...field} />
               </FormControl>
               <FormDescription>
-                This is your email used to sign in.
+                This is your email used to sign in to our app.
               </FormDescription>
               <FormMessage />
             </FormItem>
@@ -61,24 +90,41 @@ const RegisterForm = () => {
         />
         <FormField
           control={form.control}
-          name="email"
+          name="password"
           render={({ field }) => (
             <FormItem>
               <FormLabel>Password</FormLabel>
               <FormControl>
-                <Input type='password' placeholder="123456" {...field} />
+                <Input type="password" {...field} />
               </FormControl>
               <FormDescription>
-                This is your password used to sign in.
+                This is your password used to sign in to our app.
               </FormDescription>
               <FormMessage />
             </FormItem>
           )}
         />
+        <FormField
+          control={form.control}
+          name="confirmPassword"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Confirm Password</FormLabel>
+              <FormControl>
+                <Input type="password" {...field} />
+              </FormControl>
+              <FormDescription>Please confirm your password</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Link className="block" href={"/login"}>
+          Already have an account?
+        </Link>
         <Button type="submit">Submit</Button>
       </form>
     </Form>
-  )
-}
+  );
+};
 
-export default RegisterForm
+export default RegisterForm;
